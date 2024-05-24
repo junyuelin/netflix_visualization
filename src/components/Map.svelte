9a01c2b0013635fd0dc0410ffa8093e8d3fd75ef
junyuelin <junyuelin608@gmail.com>
2024-05-24 00:04:09 -0700

    added expansion_countries.csv

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -1,8 +1,8 @@
 <script>
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
-  import * as topojson from 'topojson';
-  
+  import * as topojson from 'topojson-client';
+
   onMount(() => {
     drawMap();
   });
@@ -11,19 +11,19 @@
     // Set up your SVG container
     const svg = d3.select('.map-container')
       .append('svg')
-      .attr('width', 800)
-      .attr('height', 400);
+      .attr('width', 1200)
+      .attr('height', 800);
 
-    // Define your projection (e.g., Mercator)
-    const projection = d3.geoMercator()
-      .scale(100)
-      .translate([400, 200]);
+    // Define your projection (e.g., Natural Earth)
+    const projection = d3.geoNaturalEarth1() // Using a different projection for a different map
+      .scale(150)
+      .translate([600, 400]); // Adjusted translation for centering
 
     // Define a path generator
     const path = d3.geoPath().projection(projection);
 
     // Load world map data
-    d3.json('https://raw.githubusercontent.com/d3/d3.github.com/master/world-50m.v1.json')
+    d3.json('https://raw.githubusercontent.com/d3/d3.github.com/master/world-110m.v1.json')
       .then(function(world) {
         // Draw the map
         svg.selectAll('path')
